7d73af204cc8b5bdad989a89619986a61df9a130
Lingjia Liu <gmutoo@gmail.com>
2020-02-05 15:05:10 +1100

    using mono font for tags

--- src/js/ads.js
+++ src/js/ads.js
@@ -3,7 +3,6 @@ $(() => {
         let $ad = $(el);
         if (!$ad.data('adsbygoogle-status')) {
             (adsbygoogle = window.adsbygoogle || []).push({});
-            console.log('ad++');
         }
     });
 });
